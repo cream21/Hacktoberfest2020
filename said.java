dc4c70112875afe79d702f51067912b41c97660c
said3012 <72248644+said3012@users.noreply.github.com>
2020-10-02 15:59:07 +0800

    Create said.java

---
+++ said.java
@@ -0,0 +1,12 @@
+public class a
+{
+   public static void main()
+   {
+    for(int x=5;x>=1;x--)
+    {
+        for (int y=x;y>0;y--)
+        System.out.print(y);
+        System.out.println();
+    }
+   }
+}
